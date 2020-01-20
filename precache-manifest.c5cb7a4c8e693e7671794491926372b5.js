8d647a57035f89aad407f9fb1307f321f921a51c
Brenden <brenden.niedermeyer@gmail.com>
2020-01-20 14:09:15 -0800

    Updates

---
+++ precache-manifest.c5cb7a4c8e693e7671794491926372b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4859e26515ac7c31cc221341909889c",
+    "url": "/student-management/index.html"
+  },
+  {
+    "revision": "cc4617dd4f434274a7e4",
+    "url": "/student-management/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "817b7ef42371aa760426",
+    "url": "/student-management/static/js/2.3fcc5251.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/student-management/static/js/2.3fcc5251.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc4617dd4f434274a7e4",
+    "url": "/student-management/static/js/main.b62b7f84.chunk.js"
+  },
+  {
+    "revision": "fa8b055a641c452a457c",
+    "url": "/student-management/static/js/runtime-main.23b3dc97.js"
+  },
+  {
+    "revision": "6e98631bd0be5adf7aeb03634fe6896f",
+    "url": "/student-management/static/media/undraw_exams_g4ow.6e98631b.svg"
+  }
+]);
